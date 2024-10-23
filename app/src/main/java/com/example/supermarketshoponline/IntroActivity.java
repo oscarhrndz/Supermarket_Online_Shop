1b27dec4f25432e6ee1c5db8e8de061640567ee2
Oscar Hernandez <oscarhernandezsoler@gmail.com>
2024-10-23 13:00:46 +0200

    first commit

---
+++ app/src/main/java/com/example/supermarketshoponline/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.supermarketshoponline;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.supermarketshoponline.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+
+
+    }
+}
